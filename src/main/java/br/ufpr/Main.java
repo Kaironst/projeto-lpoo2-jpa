01bc55cd98c3a14df481dd93be7ef78ea6f07b4b
kaironst <pedronovak4@gmail.com>
2025-09-26 17:18:12 -0300

    adicionado estrutura necessária para persistência básica

---
+++ src/main/java/br/ufpr/Main.java
@@ -0,0 +1,10 @@
+package br.ufpr;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// implementação vem aqui, acho melhor agnt fazer o diagrama de como cada coisa
+		// vai se comunicar primeiro
+	}
+
+}
